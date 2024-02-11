f0c89da65e8bf86d191387d93494435a1a902d1a
ARCHIE <ARCHIE@ARCHIE-PC>
2024-02-12 00:40:19 +0100

    Hash manager save

--- VaultLib.Core/Hashing/HashManager.cs
+++ VaultLib.Core/Hashing/HashManager.cs
@@ -4,6 +4,7 @@
 
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using VLT32Hasher = VaultLib.Core.Hashing.VLT32Hasher;
 using VLT64Hasher = VaultLib.Core.Hashing.VLT64Hasher;
 
@@ -13,16 +14,21 @@ namespace VaultLib.Core.Hashing
     {
         private static readonly Dictionary<uint, string> VltHashDictionary = new Dictionary<uint, string>();
         private static readonly Dictionary<ulong, string> Vlt64HashDictionary = new Dictionary<ulong, string>();
+        private static string HashFile;
 
         public static void LoadDictionary(string file)
         {
+            HashFile = file;
             foreach (var line in File.ReadLines(file)) AddVLT(line);
         }
 
         public static void AddVLT(string str)
         {
-            VltHashDictionary[VLT32Hasher.Hash(str)] = str;
-            Vlt64HashDictionary[VLT64Hasher.Hash(str)] = str;
+            if (!str.ToLowerInvariant().StartsWith("0x"))
+            {
+                VltHashDictionary[VLT32Hasher.Hash(str)] = str;
+                Vlt64HashDictionary[VLT64Hasher.Hash(str)] = str;
+            }
         }
 
         public static string ResolveVLT(uint hash)
@@ -33,6 +39,12 @@ namespace VaultLib.Core.Hashing
         public static string ResolveVLT(ulong hash)
         {
             return Vlt64HashDictionary.TryGetValue(hash, out var s) ? s : $"0x{hash:X16}";
+        }
+
+        public static void Save()
+        {
+            if (!string.IsNullOrEmpty(HashFile))
+                File.WriteAllLines(HashFile, VltHashDictionary.Select(x => x.Value));
         }
     }
 }
